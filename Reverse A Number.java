6fc04215a3b64553776e1781600aa9e87fa7378c
Siddhant <siddhantsharma669@gmail.com>
2021-10-25 14:26:30 +0530

    Reverse A Number

--- Reverse A Number.java
+++ Reverse A Number.java
@@ -1,20 +1,20 @@
 import java.util.*;
-  
-  public class Main{
-  
+
+public class Main {
+
   public static void main(String[] args) {
     // write your code here
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
-    int temp = n;
-    int count =0;
-    while(n>0){
+    while( n != 0){
+        int q=n%10;
         n=n/10;
-        count++;
-    };
-    
-    System.out.println(count);
-    
-    
-   }
+        System.out.println(q);
+    }
+
+
+
+
+
   }
+}
