daf5d13e3ae1f926b3338dc563b55d8b49b8f957
Siddhant <siddhantsharma669@gmail.com>
2021-10-25 14:35:33 +0530

    Inverse Of A Number

---
+++ Basics of Programming/Getting Statred/Inverse Of A Number.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) {
+  // write your code here 
+  Scanner scn = new Scanner(System.in);
+  int n = scn.nextInt();
+  
+  int op =1;
+  int od;
+  int rp;
+  int rd;
+  int count = 0;
+   
+   while( n != 0){
+     
+       od = n%10;
+       rp = od;
+       rd = op *(int)Math.pow(10, od - 1);
+       count = count +rd;
+       n=n/10;
+       op++;
+   }
+   
+   System.out.println(count);
+  
+ }
+}
