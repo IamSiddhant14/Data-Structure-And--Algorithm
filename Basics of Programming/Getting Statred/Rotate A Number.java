6b074067b07bff28195b685e04fbaf82672d79f9
Siddhant <siddhantsharma669@gmail.com>
2021-10-26 16:43:31 +0530

    Rotate A Number

---
+++ Basics of Programming/Getting Statred/Rotate A Number.java
@@ -0,0 +1,35 @@
+import java.util.*;
+   
+   public class Main{
+   
+   public static void main(String[] args) {
+     // write your code here  
+     Scanner scn = new Scanner(System.in);
+     int n =scn.nextInt();
+     int k = scn.nextInt();
+     int temp = n;
+     int count = 0;
+     while(temp != 0){
+         temp = temp/10;
+         count++;
+     }
+     //If there is an array of size 5 and k value is 8 then effectively we would only be rotating the array by 3 times only which is the dremainder of 8%5
+     k = k % count; 
+     if(k<0){
+        k=k+count;
+     }
+     while(k!= 0){
+        int r = n%10;
+        n = n/10;
+        r = r* (int) Math.pow(10, count - 1);
+        n=n+ r;
+        // count --;
+        k--;
+     }
+     
+     System.out.println(n);
+     
+    }
+   }
+   
+   
