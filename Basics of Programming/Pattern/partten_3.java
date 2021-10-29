8dba60e0f1f511e3c77902bc37c7b67705a77b4c
Siddhant <siddhantsharma669@gmail.com>
2021-10-29 10:01:19 +0530

    partten_3

--- Basics of Programming/Pattern/partten_3.java
+++ Basics of Programming/Pattern/partten_3.java
@@ -7,7 +7,7 @@ public class partten_3 {
         int n = scn.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
+            for (int j = 1; j <= n - i;j++) {
                 System.out.print("\t");
             }
             for (int k = 1; k <= i; k++) {
