38dd5e841f0ac86758edad1969aa71f6ee2371b7
Siddhant <siddhantsharma669@gmail.com>
2021-11-02 12:17:48 +0530

    partten_13

---
+++ Basics of Programming/Pattern/partten_13.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class partten_13 {
+    
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+
+    int n = scn.nextInt();
+
+    for (int i = 0; i <= n; i++) {
+        int val = 1;
+      for ( int j = 0; j <= i; j++) {
+        System.out.print(val+ "\t");
+         val = (val * (i - j)) / (j + 1);
+      }
+      System.out.println();
+    }
+  scn.close();
+  }
+}
+
