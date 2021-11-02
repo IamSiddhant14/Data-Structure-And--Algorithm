152872fd92e5eb68f8aff681a77377c55917c95a
Siddhant <siddhantsharma669@gmail.com>
2021-11-02 11:02:12 +0530

    Partten_11

---
+++ Basics of Programming/Pattern/partten_11.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class partten_11 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int count = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(count + "\t");
+                count += 1;
+            }
+            System.out.println();
+        }
+
+        scn.close();
+    }
+
+}
