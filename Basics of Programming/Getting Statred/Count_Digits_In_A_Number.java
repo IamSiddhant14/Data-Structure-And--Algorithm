2167310e3592f2ba6726a4d7a1fd57945c506fa8
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2021-11-10 15:10:15 +0530

    Update Count_Digits_In_A_Number.java

--- Basics of Programming/Getting Statred/Count_Digits_In_A_Number.java
+++ Basics of Programming/Getting Statred/Count_Digits_In_A_Number.java
@@ -3,7 +3,9 @@ import java.util.*;
   public class Count_Digits_In_A_Number{
   
   public static void main(String[] args) {
-    // write your code here
+    // we will take the value of number as input now assign it to a variable lets say n in this case now create 
+    // create a variable count and assign it the value as 1 now run a while lopp until the value of n became zero 
+    // in each iteration of while loop n=n/10 and count ++
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
     // int temp = n;
