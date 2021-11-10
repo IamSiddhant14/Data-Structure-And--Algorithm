8a4ff6bfbf0a6b96ca8c40ae8e525c383b192150
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2021-11-10 18:03:49 +0530

    Update Gcd_And_Lcm.java

--- Basics of Programming/Getting Statred/Gcd_And_Lcm.java
+++ Basics of Programming/Getting Statred/Gcd_And_Lcm.java
@@ -3,7 +3,10 @@ import java.util.*;
 public class Gcd_And_Lcm {
 
   public static void main(String[] args) {
-    // write your code here
+    // Here in this question we are required to calculate the gcd and lcm of two input numbers ,for gcd we are required to iterate from samller number -1 uptill 1 any number in bewtween them which devides both the number
+    // is the gcd
+    //For lcm we will take a variable j initalized by 1 after that we will iterate till n for each iteration a*j % b == 0 will result in the the lcm of the two number and we would print that
+    
     Scanner scn = new Scanner(System.in);
     int a = scn.nextInt();
     int b = scn.nextInt();
