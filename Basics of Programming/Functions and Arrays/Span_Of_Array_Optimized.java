2d8df1485a56be9c41b02d0cb754ca2d3122102f
Siddhant <siddhantsharma669@gmail.com>
2021-11-12 20:08:36 +0530

    span of a number optimized

---
+++ Basics of Programming/Functions and Arrays/Span_Of_Array_Optimized.java
@@ -0,0 +1,37 @@
+// import java.io.*;
+import java.util.*;
+
+public class Span_Of_Array_Optimized {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+
+        }
+
+        System.out.println(max - min);
+
+        scn.close();
+
+    }
+}
