8d92a790502c0e8d1eb997d8e2462ca817b5238a
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2021-11-13 14:07:32 +0530

    Update Digit_Frequency.java

--- Basics of Programming/Functions and Arrays/Digit_Frequency.java
+++ Basics of Programming/Functions and Arrays/Digit_Frequency.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
 public class Digit_Frequency {
-
+    //Here in this question we are suppose to calculate the digit frequecy of the provided digit so  as to accomplis this we will take a variable count initalze it with 0 and will 
+    // keep on increasing its value until temp which was n initale becames equal to zero ,then after that we will return the count 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
