815c9e3ad4f797f427a745e52f633c97207fd726
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2021-11-14 14:13:50 +0530

    Update Span_Of_Array.java

--- Basics of Programming/Functions and Arrays/Span_Of_Array.java
+++ Basics of Programming/Functions and Arrays/Span_Of_Array.java
@@ -2,10 +2,12 @@
 import java.util.*;
 
 public class Span_Of_Array {
-
+//Here in this question we are supoose to calucate the diffrence bewteen the max nad the minmum value diffence in an array to do that initalize a max varible ny min value and a 
+// min integer with max value after that iterate till i = 0 and i less then arr.length in each iteration compare the corresponding value with min and max and change it accordinly
+// after completion of loop return the value of max-min ;
     public static void main(String[] args) throws Exception {
 
-        Scanner scn = new Scanner(System.in);
+        Scanner scn = new Scanner(System.in); 
         int n = scn.nextInt();
 
         int[] arr = new int[n];
