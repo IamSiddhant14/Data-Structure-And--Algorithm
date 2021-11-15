674e04ab54cca995fa2ed83c0ba28680616da10b
Siddhant <siddhantsharma669@gmail.com>
2021-11-15 15:28:57 +0530

    Corrected the code of sum of twoarray

--- Basics of Programming/Functions and Arrays/Sum_Of_Two_Arrays.java
+++ Basics of Programming/Functions and Arrays/Sum_Of_Two_Arrays.java
@@ -1,4 +1,5 @@
-import java.io.*;
+
+// import java.io.*;
 import java.util.*;
 
 public class Sum_Of_Two_Arrays {
@@ -7,71 +8,64 @@ public class Sum_Of_Two_Arrays {
 
         Scanner scn = new Scanner(System.in);
 
+        // Taking input of array1 elements
         int n1 = scn.nextInt();
         int[] arr1 = new int[n1];
 
-        // populating array one
-        for (int i = 0; i < arr1.length; i++) {
+        for (int i = 0; i < n1; i++) {
+
             arr1[i] = scn.nextInt();
         }
 
+        // Taking input of array2 elements
         int n2 = scn.nextInt();
         int[] arr2 = new int[n2];
 
-        // populating array two
-        for (int j = 0; j < arr2.length; j++) {
+        for (int j = 0; j < n2; j++) {
+
             arr2[j] = scn.nextInt();
         }
 
-        int p = (n2 - 1);
-        int o = (n1 - 1);
-        int sum;
+        // Determing whic array is larger in size
+        int max = Math.max(n1, n2);
+        int sum = 0;
         int total = 0;
         int r = 0;
-        int power = 1;
+        int[] arr3 = new int[n1 > n2 ? n1 : n2];
+        int length = arr3.length;
 
-        while (p >= 0) {
+        // Running loop for maxmium number of times
+        for (int k = 1; k <= max; k++) {
 
-            int v2 = arr2[p];
-            int v1;
-            if (o >= 0) {
-                v1 = arr1[o];
-            } else {
-                v1 = 0;
+            int v1 = 0;
+            if (k <= n1) {
+                v1 = arr1[n1 - k];
+            }
+
+            int v2 = 0;
+            if (k <= n2) {
+                v2 = arr2[n2 - k];
             }
 
             sum = v1 + v2 + r;
             r = 0;
-            int q;
+            r = sum / 10;
+            sum = sum % 10;
 
-            if (sum > 9) {
-
-                r = sum % 10;
-                q = sum / 10;
-                total += (r * power);
-                r = q;
-
-            } else {
-
-                total += (sum * power);
-
-            }
-
-            o--;
-            p--;
-            power *= 10;
+            total = sum;
+            arr3[length - k] = total;
 
         }
 
-        int d = (int) Math.pow(10, (n2 - 1));
-        while (d != 0) {
-            int q = total / d;
-            System.out.println(q);
-            total = total % d;
-            d /= 10;
+        System.out.println("---------------");
+        if (r != 0) {
+            System.out.print(r + " ");
+        }
+        for (int val : arr3) {
+            System.out.print(val + " ");
         }
 
         scn.close();
-
     }
+
 }
