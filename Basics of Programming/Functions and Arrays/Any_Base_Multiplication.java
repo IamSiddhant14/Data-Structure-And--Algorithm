756818511176d5c84f2051ca5cbf854203a7f703
Siddhant <siddhantsharma669@gmail.com>
2021-11-18 14:56:29 +0530

    Second failes attempt

--- Basics of Programming/Functions and Arrays/Any_Base_Multiplication.java
+++ Basics of Programming/Functions and Arrays/Any_Base_Multiplication.java
@@ -83,3 +83,136 @@ public class Any_Base_Multiplication {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+//Second attempt
+
+
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int b = scn.nextInt();
+    int n1 = scn.nextInt();
+    int n2 = scn.nextInt();
+
+    int d = getProduct(b, n1, n2);
+    System.out.println(d);
+  }
+  
+  
+   public static int getSum(int b, int n1, int n2) {
+
+    int r = 0 ;
+    int sum = 0;
+    int total = 0 ;
+    int q ;
+    int power = 1;
+
+    while ( r != 0 || n1 != 0 || n2 != 0) {
+
+      int r1 = n1 % 10 ;
+      int r2 = n2 % 10 ;
+
+      sum = r1 + r2 + r ;
+
+    
+        r = sum / b;
+        q = sum % b;
+
+
+      total = total + (q * power) ;
+      power = power * 10 ;
+      n1 = n1 / 10 ;
+      n2 = n2 / 10 ;
+
+    }
+
+    return total ;
+
+  }
+
+  public static int getProduct(int b, int n1, int n2) {
+
+    int total = 0;
+    int gt = 0;
+    int sum = 0;
+    int sum1 = 0;
+    int sum2 = 0;
+    int power2 = 1;
+
+    while (n2 != 0) {
+      int rem = n2 % 10 ;
+      int temp = n1 ;
+      int r = 0;
+      int power = 1;
+      int rem2 = 0;
+      while ( temp != 0 || r != 0) {
+
+        rem2 = temp % 10;
+        sum = rem2 * rem + r;
+        r = 0;
+        if ( sum > b) {
+          r = sum / b;
+          sum = sum % b;
+        }
+        sum1 += sum * power;
+        temp /= 10 ;
+        power *= 10;
+
+      }
+      sum1 = sum1 * power2;
+      power2 *= 10;
+      if ( n2 != 0) {
+        n2 /= 10;
+      };
+
+
+      temp = n1 ;
+      r = 0;
+      power = 1;
+      sum = 0;
+      while ( temp != 0 || r != 0) {
+
+        rem2 = temp % 10;
+        sum = rem2 * rem + r;
+        r = 0;
+        if ( sum > b) {
+          r = sum / b;
+          sum = sum % b;
+        }
+        sum2 += sum * power;
+        temp /= 10 ;
+        power *= 10;
+
+      }
+      sum2 = sum2 * power2;
+      power2 *= 10;
+      if ( n2 != 0) {
+        n2 /= 10;
+      }
+      
+      total =getSum(b , sum1 ,sum2 );
+      gt +=total;
+
+
+
+    }
+    
+    return gt ;
+
+
+  }
+
+}
