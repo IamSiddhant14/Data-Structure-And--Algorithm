2df4f66d167fce81eb6bf5efb40a048d61608362
Siddhant <siddhantsharma669@gmail.com>
2021-12-14 02:42:22 +0530

    Print Increasing

---
+++ PEPCODING LEVEL -1/Recursion and Backtracking/introduction-to-recursion/Print_Increasing.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Print_Increasing {
+
+    public static Scanner scn = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        int n = scn.nextInt();
+        printIncreasing(n);
+
+    }
+
+    public static void printIncreasing(int n){
+
+        if( n == 0){
+            return;
+        }
+        int m = n-1;
+        printIncreasing(m);
+        System.out.println(n);
+
+        
+    }
+
+}
+
