fd8eeed1d5901272a7494b8aff01c8d008a4c057
Siddhant <siddhantsharma669@gmail.com>
2021-12-15 10:56:24 +0530

    optimized code added

--- PEPCODING LEVEL -1/Recursion and Backtracking/introduction-to-recursion/Print_Increasing_Decreasing.java
+++ PEPCODING LEVEL -1/Recursion and Backtracking/introduction-to-recursion/Print_Increasing_Decreasing.java
@@ -1,7 +1,66 @@
+
+
+
+
+
+
+// import java.io.*;
+// import java.util.*;
+
+// public class Print_Increasing_Decreasing {
+
+//     public static Scanner scn = new Scanner(System.in);
+
+//     public static void main(String[] args) throws Exception {
+        
+//         int n = scn.nextInt();
+//         pdi(n);
+//     }
+
+//     public static void inc (int n){
+
+//         if( n == 0){
+//             return;
+//         }
+ 
+//         // int m = n-1;
+//         inc( n-1);
+
+//         System.out.println(n);
+
+//     }
+
+//     public static void dec (int n){
+
+//         if( n == 0){
+//             return ;
+//         }
+
+//         System.out.println(n);
+//         dec(--n);
+
+//     }
+
+
+//     public static void pdi(int n){
+
+//         int m = n;
+
+//         dec(n);
+//         inc(m);
+
+
+
+        
+//     }
+
+// }
+
+
 import java.io.*;
 import java.util.*;
 
-public class Print_Increasing_Decreasing {
+public class Main {
 
     public static Scanner scn = new Scanner(System.in);
 
@@ -9,45 +68,40 @@ public class Print_Increasing_Decreasing {
         
         int n = scn.nextInt();
         pdi(n);
-    }
-
-    public static void inc (int n){
-
-        if( n == 0){
-            return;
-        }
- 
-        // int m = n-1;
-        inc( n-1);
-
-        System.out.println(n);
 
     }
 
-    public static void dec (int n){
+    public static void pdi(int n){
 
-        if( n == 0){
+        if ( n == 0){
             return ;
         }
 
         System.out.println(n);
-        dec(--n);
 
-    }
+        pdi(n -1);
 
-
-    public static void pdi(int n){
-
-        int m = n;
-
-        dec(n);
-        inc(m);
-
-
-
+        System.out.println(n);
         
     }
 
 }
+
+
+
+
+
+
+
+
+
+// public static void pdi(int n){
+//         if(n == 0)
+//             return;
+//         System.out.println(n); // postder dec
+//         pdi(n-1);
+//         System.out.println(n);
+        
+//     }
     
 
