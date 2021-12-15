a35c5bc5520b6e01be64fdcf88a54c64a34885fb
Siddhant <siddhantsharma669@gmail.com>
2021-12-15 12:15:51 +0530

    added optimized solution

--- PEPCODING LEVEL -1/Recursion and Backtracking/introduction-to-recursion/Power_logarithmic.java
+++ PEPCODING LEVEL -1/Recursion and Backtracking/introduction-to-recursion/Power_logarithmic.java
@@ -1,22 +1,66 @@
 
+// import java.io.*;
+// import java.util.*;
+    
+// public class Power_logarithmic {
+
+
+//     public static Scanner scn = new Scanner(System.in);
+
+//     public static void main(String[] args) throws Exception {
+        
+//         int x = scn.nextInt();
+//         int n = scn.nextInt();
+     
+
+//         System.out.println(power(x,n));
+
+//     }
+    
+
+//     public static int power(int x, int n){
+
+//         if( n == 0){
+//             return 1;
+//         }
+
+//         if( n % 2 == 0){
+
+//             int val = power( x , n/2) * power( x , n/2);
+//             return val;
+
+
+//         }else{
+
+//              int val = power( x , n/2) * power( x , n/2) ;
+//              val = x * val ;
+//              return val;
+
+//         }
+        
+//     }
+
+// }
+
+
+
+
+
 import java.io.*;
 import java.util.*;
-    
-public class Power_logarithmic {
 
+public class  Power_logarithmic {
 
     public static Scanner scn = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
+
+       int x = scn.nextInt();
+       int n = scn.nextInt();
+
+       System.out.println(power( x , n));
         
-        int x = scn.nextInt();
-        int n = scn.nextInt();
-     
-
-        System.out.println(power(x,n));
-
     }
-    
 
     public static int power(int x, int n){
 
@@ -24,21 +68,22 @@ public class Power_logarithmic {
             return 1;
         }
 
-        if( n % 2 == 0){
+        int val = power( x , n/2);
 
-            int val = power( x , n/2) * power( x , n/2);
-            return val;
+        int final_ans = val * val ;
 
+        if( n % 2 != 0){
 
-        }else{
-
-             int val = power( x , n/2) * power( x , n/2) ;
-             val = x * val ;
-             return val;
+            final_ans = final_ans * x;
 
         }
-        
+
+        return final_ans;
+
+
     }
 
 }
+
+
     
