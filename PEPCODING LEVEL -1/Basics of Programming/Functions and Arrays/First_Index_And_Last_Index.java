900347bdbbb1425e7d56deb76748fc70a29c672f
Siddhant <siddhantsharma669@gmail.com>
2021-12-18 22:20:02 +0530

    First Index And Last Index

---
+++ PEPCODING LEVEL -1/Basics of Programming/Functions and Arrays/First_Index_And_Last_Index.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class First_Index_And_Last_Index {
+public static void main(String[] args) throws Exception {
+    
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    
+    int[] arr = new int[n];
+    
+    for( int i =0; i<n ; i++){
+        arr[i] = scn.nextInt();
+    }
+    
+    int d = scn.nextInt();
+    
+    int li = -1;
+    int hi = li;
+    
+    for( int i = 0; i< n ; i++){
+        
+        if( arr[i] == d ){
+            if( li == -1){
+                li = i;
+            }
+            hi = i;
+        }
+    }
+    
+    System.out.println(li);
+    System.out.println(hi);
+    
+ }
+
+}
