f6e702d6591c26e2e8ee6aad059d8cadab06957f
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2021-12-28 00:58:52 +0530

    updated

--- PEPCODING LEVEL -1/Basics of Programming/Getting Statred/Prime_Factorization_Of_A_Number_optimized.java
+++ PEPCODING LEVEL -1/Basics of Programming/Getting Statred/Prime_Factorization_Of_A_Number_optimized.java
@@ -30,3 +30,31 @@ public static void main(String[] args) {
 }
 
 
+
+
+
+
+//Approch _2
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) {
+  Scanner scn = new Scanner(System.in);
+  int n = scn.nextInt();
+
+  for(int i =2; i* i <= n; i++){
+
+      while( n % i == 0){
+          n = n / i;
+          System.out.print(i+" ");
+      }
+  }
+
+  if( n != 1){
+      System.out.print(n);
+  }
+ }
+}
+
+
