a74c5c255b2e1c72b74c466d3dc8d3bd41158766
Siddhant <siddhantsharma669@gmail.com>
2022-01-01 12:18:19 +0530

    One line Approch ,maxOfArray

---
+++ PEPCODING LEVEL -1/Recursion and Backtracking/recursion-in-arrays/maxOfArray_third_approch.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class maxOfArray_third_approch {
+
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+
+        for( int i = 0; i<n; i++){
+            arr[i] = scn.nextInt();
+        }
+
+        System.out.println(maxOfArray(arr,0));
+    }
+
+    public static int maxOfArray(int[] arr, int idx){
+
+        // One line Approch :-
+
+        return (idx == arr.length - 1) ? arr[idx] : Math.max( arr[idx] : maxOfArray(arr, idx + 1 ));
+        
+        
+    }
+
+
+
+
+    
+}
