7c4a05dd492e6bc13e75b378b0f5e1c0384a297a
Siddhant <siddhantsharma669@gmail.com>
2022-01-06 01:26:34 +0530

    Get Stair Paths

---
+++ PEPCODING LEVEL -1/Recursion and Backtracking/Recursion with Arraylists/Get_Stair_Paths.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Get_Stair_Paths {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        ArrayList<String> a = getStairPaths(n);
+        System.out.println(a);
+
+    }
+
+    public static ArrayList<String> getStairPaths(int n) {
+
+        if( n == 0 ){
+            
+            ArrayList<String> a = new ArrayList<>();
+            a.add("");
+            return a;
+
+        }
+
+        ArrayList<String> ans = new ArrayList<>();
+
+        if( n-1 >= 0){
+            ArrayList<String> num1 = getStairPaths(n-1);
+            for( String ele : num1){
+                ans.add("1"+ele);
+            }
+        }
+
+        if( n-2 >= 0){
+            ArrayList<String> num2 = getStairPaths(n-2);
+            for( String ele : num2){
+                ans.add("2"+ele);
+            }
+        }
+
+        if( n-3 >= 0){
+            ArrayList<String> num3 = getStairPaths(n-3);
+            for( String ele : num3){
+                ans.add("3"+ele);
+            }
+        }
+
+
+        return ans;
+        
+        
+        
+    }
+
+}
+    
+
