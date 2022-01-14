f6777341624aeb034bd4c03d644ea4f56ffab265
Siddhant <siddhantsharma669@gmail.com>
2022-01-14 11:43:42 +0530

    The Curious Case Of Benjamin Bulbs

---
+++ PEPCODING LEVEL -1/1.0_Basics of Programming/Getting Statred/The_Curious_Case_Of_Benjamin_Bulbs.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class The_Curious_Case_Of_Benjamin_Bulbs {
+  public static void main(String[] args) {
+     
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    for(int i=1 ; i*i< n; i++){
+        System.out.println(i*i);
+    }
+
+   }
+}
+    
+
