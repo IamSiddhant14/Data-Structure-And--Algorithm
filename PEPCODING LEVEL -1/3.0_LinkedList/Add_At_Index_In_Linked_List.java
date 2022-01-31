3f34be1c0b5ed540b5a319e212e4483b0ea90b60
Siddhant <siddhantsharma669@gmail.com>
2022-02-01 01:05:14 +0530

    not final solution , bugs present

---
+++ PEPCODING LEVEL -1/3.0_LinkedList/Add_At_Index_In_Linked_List.java
@@ -0,0 +1,194 @@
+   
+import java.io.*;
+import java.util.*;
+
+public class Add_At_Index_In_Linked_List {
+ 
+  public static class Node {
+    int data;
+    Node next;
+  }
+
+  public static class LinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    void addLast(int val) {
+      Node temp = new Node();
+      temp.data = val;
+      temp.next = null;
+
+      if (size == 0) {
+        head = tail = temp;
+      } else {
+        tail.next = temp;
+        tail = temp;
+      }
+
+      size++;
+    }
+
+    public int size() {
+      return size;
+    }
+
+    public void display() {
+      for (Node temp = head; temp != null; temp = temp.next) {
+        System.out.print(temp.data + " ");
+      }
+      System.out.println();
+    }
+
+    public void removeFirst() {
+      if (size == 0) {
+        System.out.println("List is empty");
+      } else if (size == 1) {
+        head = tail = null;
+        size = 0;
+      } else {
+        head = head.next;
+        size--;
+      }
+    }
+
+    public int getFirst() {
+      if (size == 0) {
+        System.out.println("List is empty");
+        return -1;
+      } else {
+        return head.data;
+      }
+    }
+
+    public int getLast() {
+      if (size == 0) {
+        System.out.println("List is empty");
+        return -1;
+      } else {
+        return tail.data;
+      }
+    }
+
+    public int getAt(int idx) {
+      if (size == 0) {
+        System.out.println("List is empty");
+        return -1;
+      } else if (idx < 0 || idx >= size) {
+        System.out.println("Invalid arguments");
+        return -1;
+      } else {
+        Node temp = head;
+        for (int i = 0; i < idx; i++) {
+          temp = temp.next;
+        }
+        return temp.data;
+      }
+    }
+
+    public void addFirst(int val) {
+      Node temp = new Node();
+      temp.data = val;
+      temp.next = head;
+      head = temp;
+      
+      if(size == 0){
+        tail = temp;
+      }
+
+      size++;
+    }
+
+    public void addAt(int idx, int val){
+        
+    Node node = new Node();
+    node.data = val;
+    
+      if( idx < 0 && idx > size){
+          System.out.println("Invalid arguments");
+          return ;
+          
+      }else if( size == 0){
+          head = node;
+          tail = node;
+          node.next = null;
+          
+       }else if( size == 1 && idx == 0){
+          node.next = head;
+          head = node;
+          
+       }else if( size == 1 && idx == 1){
+           tail.next = node;
+           node.next = null;
+          
+        }else if( idx == 0){
+          node.next = head;
+          head = node;
+          
+       }else if( idx == size  ){
+          tail.next = node;
+          node.next = null;
+          
+       }else{
+          Node curr = head;
+          for( int i =0; i<idx-1 ; i++){
+              curr = curr.next;
+          }
+          
+        //   Node tail = curr.next;
+        //   curr.next = node;
+        //   node.next = tail; 
+          
+          node.next = curr.next;
+          curr.next = node;
+          
+      }
+      
+      size++;
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    LinkedList list = new LinkedList();
+
+    String str = br.readLine();
+    while (str.equals("quit") == false) {
+      if (str.startsWith("addLast")) {
+        int val = Integer.parseInt(str.split(" ")[1]);
+        list.addLast(val);
+      } else if (str.startsWith("size")) {
+        System.out.println(list.size());
+      } else if (str.startsWith("display")) {
+        list.display();
+      } else if (str.startsWith("removeFirst")) {
+        list.removeFirst();
+      } else if (str.startsWith("getFirst")) {
+        int val = list.getFirst();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("getLast")) {
+        int val = list.getLast();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("getAt")) {
+        int idx = Integer.parseInt(str.split(" ")[1]);
+        int val = list.getAt(idx);
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("addFirst")) {
+        int val = Integer.parseInt(str.split(" ")[1]);
+        list.addFirst(val);
+      } else if (str.startsWith("addAt")) {
+        int idx = Integer.parseInt(str.split(" ")[1]);
+        int val = Integer.parseInt(str.split(" ")[2]);
+        list.addAt(idx, val);
+      } 
+      str = br.readLine();
+    }
+  }
+}
+
