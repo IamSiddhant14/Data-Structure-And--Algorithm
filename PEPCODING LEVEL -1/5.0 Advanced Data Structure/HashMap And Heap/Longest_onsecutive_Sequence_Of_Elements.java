7dfd5177846f3d2fcdf3f5dee0b0c6b00f87751e
Siddhant <siddhantsharma669@gmail.com>
2022-02-20 17:04:10 +0530

    Longest Consecutive Sequence Of Elements

---
+++ PEPCODING LEVEL -1/5.0 Advanced Data Structure/HashMap And Heap/Longest_onsecutive_Sequence_Of_Elements.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Longest_onsecutive_Sequence_Of_Elements {
+
+public static void main(String[] args) throws Exception {
+
+   Scanner scn = new Scanner(System.in);
+   int n = scn.nextInt();
+
+   int[] arr = new int[n];
+
+   for( int i =0; i<arr.length; i++){
+     arr[i] = scn.nextInt();
+   }
+
+   HashMap <Integer , Boolean> hm = new HashMap<>();
+
+   for( int ele : arr ){
+      hm.put( ele , true);
+   }
+
+   for( int ele : hm.keySet() ){
+      if( hm.containsKey(ele-1)){
+          hm.put(ele , false);
+      }
+   }
+
+   int maxLen = 0;
+   int MaxStPt = 0;
+
+   for( int ele : hm.keySet() ){
+
+       if( hm.get(ele) == true ){
+
+           int tempLen = 1;
+           int tempStPt = ele;;
+
+           while( hm.containsKey(tempStPt+tempLen)){
+               tempLen++;
+           }
+
+           if( tempLen> maxLen ){
+               MaxStPt = ele;
+               maxLen = tempLen;
+           }  
+       }
+   }
+
+   for( int i = MaxStPt; i<MaxStPt+maxLen;i++){
+       System.out.println(i);
+   }
+
+
+
+ }
+
+}
