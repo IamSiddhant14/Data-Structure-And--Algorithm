213d681dc82fd930908a50d9684567ab82eb4b20
Siddhant <siddhantsharma669@gmail.com>
2022-02-20 17:06:41 +0530

    Added comments

--- PEPCODING LEVEL -1/5.0 Advanced Data Structure/HashMap And Heap/Longest_onsecutive_Sequence_Of_Elements.java
+++ PEPCODING LEVEL -1/5.0 Advanced Data Structure/HashMap And Heap/Longest_onsecutive_Sequence_Of_Elements.java
@@ -21,7 +21,7 @@ public static void main(String[] args) throws Exception {
    }
 
    for( int ele : hm.keySet() ){
-      if( hm.containsKey(ele-1)){
+      if( hm.containsKey(ele-1)){//Lets suppose our ans is 5,6,7,8,9 so here there is no point in taking the static point as 6 thats why if 5 exist then mark 6 as false which means it cannot became tempory static point as five already exist
           hm.put(ele , false);
       }
    }
