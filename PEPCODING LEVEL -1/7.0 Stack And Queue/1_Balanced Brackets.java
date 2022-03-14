7e5e457eb3351998704a2162396feadbe36a0634
Siddhant <siddhantsharma669@gmail.com>
2022-03-14 19:24:44 +0530

    Added

--- PEPCODING LEVEL -1/7.0 Stack And Queue/1_Balanced Brackets.java
+++ PEPCODING LEVEL -1/7.0 Stack And Queue/1_Balanced Brackets.java
@@ -1,46 +1,54 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-
-  public static Boolean ans( String s ) {
-
-    Stack<Character> st = new Stack<>();
-
-    for ( int i = 0; i < s.length() ; i++) {
-
-      char ch = s.charAt(i);
-      if ( ch == '(' || ch == '{' || ch == '[') {
-          
-        st.push(ch);
-      } else if (  ch == ')' || ch == '}' ||  ch == ']' ) {
-          
-        if ( st.size() == 0 )
-          return false;
-        else if ( ch == ')' && st.peek() != '(')
-          return false;
-        else if ( ch == '}' && st.peek() != '{')
-          return false;
-        else if ( ch == ']' && st.peek() != '[')
-          return false;
-        else {
-          st.pop();
-        }
-        
-      }
-
-    }
-    // System.out.println( st.size() );
-    return st.size() == 0;
-
-  }
+ class Main {
 
   public static void main(String[] args) throws Exception {
 
     Scanner scn = new Scanner(System.in);
-    String s = scn.next();
+    String s = scn.nextLine();
 
-    System.out.println(ans(s));
+    Boolean ans = true;
+
+    Stack<Character> st = new Stack<>();
+
+    for (int i = 0; i < s.length(); i++) {
+
+      char ch = s.charAt(i);
+
+      if (ch == '(' || ch == '{' || ch == '[') {
+
+        st.push(ch);
+
+      } else if ((ch >= 'a' && ch <= 'z') || ch == '+' || ch == '-' || ch == '/' || ch == '*' || ch == ' ') {
+        // System.out.println("char"+ch);
+        continue;
+      }
+
+      else {
+
+        if (st.size() == 0) {
+          ans = false;
+          break;
+        } else if (ch == ')' && st.peek() != '(') {
+          ans = false;
+          ;
+          break;
+        } else if (ch == '}' && st.peek() != '{') {
+          ans = false;
+          break;
+        } else if (ch == ']' && st.peek() != '[') {
+          ans = false;
+          break;
+        } else {
+          st.pop();
+        }
+      }
+    }
+
+    ans = (st.size() == 0);
+
+    System.out.println(ans);
 
   }
 
--- PEPCODING LEVEL -1/7.0 Stack And Queue/2_DuplicateBrackets.java
+++ PEPCODING LEVEL -1/7.0 Stack And Queue/2_DuplicateBrackets.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class DuplicateBrackets {
+ class DuplicateBrackets {
 
     public static void main(String[] args) throws Exception {
         
