07f17b577ef071a974e5d81ca6c208746df058d9
Siddhant <siddhantsharma669@gmail.com>
2022-03-14 21:51:11 +0530

    Next Smaller Element To The Left

---
+++ PEPCODING LEVEL -1/7.0 Stack And Queue/7.Next Smaller Element To The Left.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+ class Main {
+  public static void display(int[] a) {
+    StringBuilder sb = new StringBuilder();
+
+    for (int val : a) {
+      sb.append(val + "\n");
+    }
+    System.out.println(sb);
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for (int i = 0; i < n; i++) {
+      a[i] = Integer.parseInt(br.readLine());
+    }
+
+    int[] ngl = solve(a);
+    display(ngl);
+  }
+
+  public static int[] solve(int[] arr) {
+    int n = arr.length;
+    int [] ans = new int[n];
+    
+    Arrays.fill(ans , -1);
+    
+    Stack<Integer> st = new Stack<>();
+    for( int i =n-1; i>= 0 ; i--){
+        
+        while( st.size() != 0 && arr[st.peek()] > arr[i]){
+            int idx = st.pop() ;
+            ans[idx] = arr[i] ;
+        }
+        st.push(i);
+    }
+   
+     return ans;
+ 
+ 
+ 
+  }
+
+}
