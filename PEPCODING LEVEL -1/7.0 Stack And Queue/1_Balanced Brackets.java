3409d4302a66bac25859e5c1e9aebc901448bd40
Siddhant <siddhantsharma669@gmail.com>
2022-03-16 16:50:14 +0530

    Added

--- PEPCODING LEVEL -1/7.0 Stack And Queue/1_Balanced Brackets.java
+++ PEPCODING LEVEL -1/7.0 Stack And Queue/1_Balanced Brackets.java
@@ -3,53 +3,49 @@ import java.util.*;
 
  class Main {
 
-  public static void main(String[] args) throws Exception {
-
-    Scanner scn = new Scanner(System.in);
-    String s = scn.nextLine();
-
-    Boolean ans = true;
-
-    Stack<Character> st = new Stack<>();
-
-    for (int i = 0; i < s.length(); i++) {
-
-      char ch = s.charAt(i);
-
-      if (ch == '(' || ch == '{' || ch == '[') {
-
-        st.push(ch);
-
-      } else if ((ch >= 'a' && ch <= 'z') || ch == '+' || ch == '-' || ch == '/' || ch == '*' || ch == ' ') {
-        // System.out.println("char"+ch);
-        continue;
-      }
-
-      else {
-
-        if (st.size() == 0) {
-          ans = false;
-          break;
-        } else if (ch == ')' && st.peek() != '(') {
-          ans = false;
-          ;
-          break;
-        } else if (ch == '}' && st.peek() != '{') {
-          ans = false;
-          break;
-        } else if (ch == ']' && st.peek() != '[') {
-          ans = false;
-          break;
-        } else {
-          st.pop();
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scn = new Scanner(System.in);
+        String s = scn.nextLine();
+        
+        boolean ans = true;
+        
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i<s.length() ; i++){
+            char ch = s.charAt(i);
+            
+            if( ch == '[' || ch == '(' || ch == '{'){
+                st.push(ch);
+            }else{
+                
+                if( ch == ')' || ch == '}' || ch == ']'){
+                    
+                    if( st.size() == 0){
+                        ans = false;
+                        break;
+                    }else if ( ch == ')' &&  st.peek() != '('){
+                        ans = false;
+                        break;
+                    }else if ( ch == '}' &&  st.peek() != '{'){
+                        ans = false;
+                        break;
+                    }else if ( ch == ']' &&  st.peek() != '['){
+                        ans = false;
+                        break;
+                    }else{
+                        st.pop();
+                    }
+                    
+                }else{
+                    continue;
+                }
+                
+                
+            }
         }
-      }
+        ans = (st.size() == 0) && (ans) ;
+        System.out.println(ans);
+
     }
-
-    ans = (st.size() == 0);
-
-    System.out.println(ans);
-
-  }
 
 }
