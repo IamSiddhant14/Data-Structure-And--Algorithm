6d5f60e9fd214b4b980bad911bb7efe752af0593
Siddhant <siddhantsharma669@gmail.com>
2022-03-20 00:41:38 +0530

    Added

--- PEPCODING LEVEL -1/7.0 Stack And Queue/infixEvalutaion.java
+++ PEPCODING LEVEL -1/7.0 Stack And Queue/infixEvalutaion.java
@@ -1,108 +1,111 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 
 public class infixEvalutaion {
 
-  public static boolean isOptr (char ch) {
+    public static boolean isOpr( char s ){
 
-    if ( ch == '+' || ch == '-' || ch == '*' || ch == '/') {
-      return true;
+        if( s == '+' || s == '-' || s == '*' || s == '/'){
+            return true;
+        }
+
+        return false;
     }
 
-    return false;
+    public static int precedence(char ch){
 
-  }
+        if( ch == '+' || ch == '-'){
+            return 1;
+        }
 
-  public static int calVal( int a, int b, char optr ) {
-
-    if (optr == '+') {
-      return a + b;
-    }
-    else if (optr == '-') {
-      return a - b;
-    }
-    else if (optr == '*') {
-      return a * b;
-    }
-    else if (optr == '/') {
-      return a / b;
-    }
-    return 0;
-  }
-
-  public static int precedence(char opr ) {
-    if ( opr == '+' || opr == '-') {
-      return 1;
+        return 2;
     }
 
-    return 2;
-  }
+    public static int vCal ( int v1 , int v2 , char optr ){
+        int sum = 0;
 
+        if( optr == '+' ){
+            sum = v1 + v2;
 
+        }else if( optr == '-' ){
+            sum = v1 - v2;
 
+        }else if( optr == '*' ){
+            sum = v1 * v2;
 
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String exp = br.readLine();
-
-    Stack<Character> operator = new Stack<>();
-    Stack<Integer> operand = new Stack<>();
-
-    for (char ch : exp.toCharArray() ) {
-
-      if ( ch == '(' ) {
-
-        operator.push(ch);
-
-      } else if ( ch >= '0' && ch <= '9' ) {
-
-        operand.push(ch - '0');
-
-      } else if (  ch == ')' ) {
-
-        while ( operator.peek() != '(') {
-
-          char optr = operator.pop();
-
-          int v2 = operand.pop();
-          int v1 = operand.pop();
-
-          int ans = calVal(v1, v2, optr);
-          operand.push(ans);
+        }else if( optr == '/' ){
+            sum = v1 / v2;
 
         }
 
-        operator.pop();
+        return sum;
 
-      } else if ( isOptr(ch) ) {
+    }
 
-        while ( operator.size() > 0 && operator.peek() != '(' && precedence(ch) <= precedence(operator.peek())) {
+    public static void main( String [] args ){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String exp = scn.nextLine();
 
-          char optr = operator.pop();
+        Stack<Character> operator = new Stack<> () ;
+        Stack<Integer> operand = new Stack<> () ;
 
-          int v2 = operand.pop();
-          int v1 = operand.pop();
+        for( char ch : exp.toCharArray() ){
 
-          int ans = calVal(v1, v2, optr);
-          operand.push(ans);
+            if( ch == '(' ){
+                operator.push(ch);
+
+            }else if( ch == ')' ){
+                
+                while( operand.peek() != '('  ){
+                    
+                    char optr = operator.pop();
+
+                    int v2 = operand.pop();
+                    int v1 = operand.pop();
+
+                    int num = vCal( v1 , v2 , optr);
+                    operand.push(num);
+                }
+
+                operator.pop();
+
+            }else if ( ch >= '0' && ch <= '9'){
+                operand.push(ch - '0');
+
+            }else if( isOpr(ch) ){
+
+                while( operator.size() != 0 && operator.peek() != '(' && precedence(ch) <= precedence(operator.peek()) ){
+
+                    char optr = operator.pop();     //+-x/
+                    int v2 = operand.pop();
+                    int v1 = operand.pop();
+                    int ans = vCal(v1,v2,optr);
+                    operand.push(ans);
+
+                }
+
+                operator.push(ch);
+
+
+
+            }
+
+
 
         }
 
-      }
-
-
+        while(operator.size () != 0){
+            char optr = operator.pop();     //+-x/
+            int v2 = operand.pop();
+            int v1 = operand.pop();
+            int ans = vCal(v1,v2,optr);
+            operand.push(ans);
+        }
+        
+        System.out.println(operand.peek());
     }
+
     
-    while (operator.size () != 0) {
-      char optr = operator.pop();    
-      int v2 = operand.pop();
-      int v1 = operand.pop();
-      int ans = calVal(v1, v2, optr);
-      operand.push(ans);
-    }
 
-    System.out.println(operand.peek());
-
-  }
 }
-
